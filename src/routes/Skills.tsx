b00a70a14593393c9f068379c6ee517c65a3c655
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-01-04 17:45:43 -0300

    Corregir tamaño fotos skills

--- src/routes/Skills.tsx
+++ src/routes/Skills.tsx
@@ -77,7 +77,7 @@ import Radio  from '@mui/material/Radio';
             </div>
             <div className='box-skills'>
               <div className='tittle-skills'>Javascript</div>
-              <img src="src/img/logos/javascript.png" alt="Javascript" />
+              <img src="src/img/logos/javascript.png" alt="Javascript" className='img-javascript'/>
               <div className='points-skills'>
                 <Radio
                   checked={true}
@@ -108,7 +108,7 @@ import Radio  from '@mui/material/Radio';
             </div>
             <div className='box-skills'>
               <div className='tittle-skills'>React</div>
-              <img src="src/img/logos/react.png" alt="Reactjs" />
+              <img src="src/img/logos/react.png" alt="Reactjs" className='img-react'/>
               <div className='points-skills'>
                 <Radio
                   checked={true}
@@ -139,7 +139,7 @@ import Radio  from '@mui/material/Radio';
             </div>
             <div className='box-skills'>
               <div className='tittle-skills'>NodeJS</div>
-              <img src="src/img/logos/nodejs.png" alt="NodeJs" />
+              <img src="src/img/logos/nodejs.png" alt="NodeJs" className='img-nodejs'/>
               <div className='points-skills'>
                 <Radio
                   checked={true}
@@ -170,7 +170,7 @@ import Radio  from '@mui/material/Radio';
             </div>
             <div className='box-skills'>
               <div className='tittle-skills'>Python</div>
-              <img src="src/img/logos/python.png" alt="Python" />
+              <img src="src/img/logos/python.png" alt="Python" className='img-python'/>
               <div className='points-skills'>
                 <Radio
                   checked={true}
@@ -201,7 +201,7 @@ import Radio  from '@mui/material/Radio';
             </div>
             <div className='box-skills'>
               <div className='tittle-skills'>MySQL</div>
-              <img src="src/img/logos/mysql.png" alt="Mysql" />
+              <img src="src/img/logos/mysql.png" alt="Mysql" className='img-mysql'/>
               <div className='points-skills'>
                 <Radio
                   checked={true}
@@ -232,7 +232,38 @@ import Radio  from '@mui/material/Radio';
             </div>
             <div className='box-skills'>
               <div className='tittle-skills'>Git-Github</div>
-              <img src="src/img/logos/git-github.png" alt="Git-Github" />
+              <img src="src/img/logos/git-github.png" alt="Git-Github" className='img-gitgithub' />
+              <div className='points-skills'>
+                <Radio
+                  checked={true}
+                  color='success'
+                  size='medium'
+                />
+                <Radio
+                  checked={true}
+                  color='success'
+                  size='medium'
+                />
+                <Radio
+                  checked={true}
+                  color='success'
+                  size='medium'
+                />
+                <Radio
+                  checked={true}
+                  color='success'
+                  size='medium'
+                />
+                <Radio
+                  checked={true}
+                  color='success'
+                  size='medium'
+                />
+              </div>
+            </div>
+            <div className='box-skills'>
+              <div className='tittle-skills'>Typescript</div>
+              <img src="src/img/logos/typescript.png" alt="Typescript" className='img-typescript'/>
               <div className='points-skills'>
                 <Radio
                   checked={true}
