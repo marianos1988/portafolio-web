7b527165a19a53ad4b7634d962864724191f4408
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-01-09 17:09:55 -0300

    Agregar imagenes en carrousel sys-gym

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -13,23 +13,47 @@ export const Carousel = ({ project }: Props) => {
   return (
     <>
       <div className="carousel-wrapper">
-        <div id="item-1"></div>
-        <div id="item-2"></div>
-        <div id="item-3"></div>
+        <div id="item-1-sys-gym"></div>
+        <div id="item-2-sys-gym"></div>
+        <div id="item-3-sys-gym"></div>
+        <div id="item-4-sys-gym"></div>
+        <div id="item-5-sys-gym"></div>
+        <div id="item-6-sys-gym"></div>
+        <div id="item-7-sys-gym"></div>
 
-        <div className="carousel-item item-1">
-          <a href="#item-3" className="arrow-prev arrow"></a>
-          <a href="#item-2" className="arrow-next arrow"></a>
+        <div className="carousel-item item-1-sys-gym">
+          <a href="#item-7-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-2-sys-gym" className="arrow-next arrow"></a>
         </div>
 
-        <div className="carousel-item item-2">
-          <a href="#item-1" className="arrow-prev arrow"></a>
-          <a href="#item-3" className="arrow-next arrow"></a>
+        <div className="carousel-item item-2-sys-gym">
+          <a href="#item-1-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-3-sys-gym" className="arrow-next arrow"></a>
         </div>
 
-        <div className="carousel-item item-3">
-          <a href="#item-2" className="arrow-prev arrow"></a>
-          <a href="#item-1" className="arrow-next arrow"></a>
+        <div className="carousel-item item-3-sys-gym">
+          <a href="#item-2-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-4-sys-gym" className="arrow-next arrow"></a>
+        </div>
+
+        <div className="carousel-item item-4-sys-gym">
+          <a href="#item-3-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-5-sys-gym" className="arrow-next arrow"></a>
+        </div>
+
+        <div className="carousel-item item-5-sys-gym">
+          <a href="#item-4-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-6-sys-gym" className="arrow-next arrow"></a>
+        </div>
+
+        <div className="carousel-item item-6-sys-gym">
+          <a href="#item-5-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-7-sys-gym" className="arrow-next arrow"></a>
+        </div>
+
+        <div className="carousel-item item-7-sys-gym">
+          <a href="#item-6-sys-gym" className="arrow-prev arrow"></a>
+          <a href="#item-1-sys-gym" className="arrow-next arrow"></a>
         </div>
 
       </div>
