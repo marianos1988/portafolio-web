1bef14354a5a3092e492418ae16cb799a2f257dc
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-01-11 15:04:57 -0300

    Crear propio Carousel de imagenes

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,63 +1,30 @@
-
-import "../styles/Carousel.css"
-
+import { useState } from 'react';
+import "../styles/Carousel.css";
 
 type Props = {
-  project: string
+    cant: number
 }
 
+export const Carousel = ({ cant }: Props) => {
 
-export const Carousel = ({ project }: Props) => {
+ const [cantFotos, setCantFotos ] = useState(1);
 
 
+ const nextTo = (num:number) => {
+  if(num === cant) setCantFotos(1);   
+  else setCantFotos(cantFotos+1);
+  }
+
+ 
+ const previousTo = (num:number) => {
+  if(num === 1) setCantFotos(cant);
+  else setCantFotos(cantFotos-1);
+ }
+  
   return (
-    <>
-      <div className="carousel-wrapper">
-        <div id="item-1-sys-gym"></div>
-        <div id="item-2-sys-gym"></div>
-        <div id="item-3-sys-gym"></div>
-        <div id="item-4-sys-gym"></div>
-        <div id="item-5-sys-gym"></div>
-        <div id="item-6-sys-gym"></div>
-        <div id="item-7-sys-gym"></div>
-
-        <div className="carousel-item item-1-sys-gym">
-          <a href="#item-7-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-2-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-        <div className="carousel-item item-2-sys-gym">
-          <a href="#item-1-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-3-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-        <div className="carousel-item item-3-sys-gym">
-          <a href="#item-2-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-4-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-        <div className="carousel-item item-4-sys-gym">
-          <a href="#item-3-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-5-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-        <div className="carousel-item item-5-sys-gym">
-          <a href="#item-4-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-6-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-        <div className="carousel-item item-6-sys-gym">
-          <a href="#item-5-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-7-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-        <div className="carousel-item item-7-sys-gym">
-          <a href="#item-6-sys-gym" className="arrow-prev arrow"></a>
-          <a href="#item-1-sys-gym" className="arrow-next arrow"></a>
-        </div>
-
-      </div>
-    </>
-    
+    <div className={`container-carousel carousel-sys-gym-${cantFotos}`}>
+      <img src="./src/assets/izquierda.png" alt="Left" className='left' onClick={()=>nextTo(cantFotos)}/>
+      <img src="./src/assets/derecha.png" alt="Right" className='right' onClick={()=>previousTo(cantFotos)}/>
+    </div>
   )
 }
--- src/routes/Proyectos.tsx
+++ src/routes/Proyectos.tsx
@@ -4,11 +4,6 @@ import "../styles/Proyectos.css"
 import { Carousel } from '../components/Carousel'
 
 
-
-
-
-
-
 export const Proyectos = () => {
 
   
@@ -28,11 +23,30 @@ export const Proyectos = () => {
                 Gracias a mi gran desempeño y dedicación, logré armar mi primer proyecto, un sistema de administración de socios y caja para un gimnasio. Este sistema permite administrar los pagos y la morosidad de cada socio, notifica la fecha de cumpleaños de los mismos y realiza reportes diarios de la caja descargando en PDF.
                 <br />
                 <br />
-                Link Github: https://github.com/marianos1988/sys-gym
+                Link Github: <a href="https://github.com/marianos1988/sys-gym" target="_blank">https://github.com/marianos1988/sys-gym</a>
               </p>
             </div>
             <div className="carrousel-proyecto">
-              <Carousel project={''}></Carousel>
+              <Carousel cant={7}></Carousel>
+            </div>
+          </div>
+
+          <div className='box-proyectos'>
+            <div className='desc-proyecto'>
+              <h4>Sep. 2023 - Sep. 2023</h4>
+              <h2>Sitio web www.jbombicino.com.ar</h2>
+              <p>
+                Desarrollé una pagina web estática 100% personalizada y construida con HTML5, CSS, y Javascript para la Clinica O. Bombicino.
+                <br />
+                Es una gran muestra de mis habilidades, del buen manejo que llevo para diseñar una pagina web responsive adaptable a cualquier dispositivo.
+                <br />
+                Sitio web: <a href="https://www.jbombicino.com.ar/" target="_blank">https://www.jbombicino.com.ar</a>
+                <br />
+                Link Github: <a href="https://github.com/marianos1988/web-clinica-jb" target="_blank">https://github.com/marianos1988/web-clinica-jb</a>
+              </p>
+            </div>
+            <div className="carrousel-proyecto">
+
             </div>
           </div> 
           
